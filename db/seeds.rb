e7dd35c26694e2f1ca68c1cb4569a819739de066
Anastasia Parfait <anastasia.parfait@audencia.com>
2021-11-17 16:02:24 +0100

    Finished Yeld MVP without frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+
+CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: CATEGORY.sample
+  )
+
+  2.times do
+    Review.create!(
+      rating: rand(0..5),
+      content: Faker::Lorem.paragraphs,
+      restaurant: restaurant
+    )
+  end
+end
